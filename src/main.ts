63c21145e82cabf99046d45b09c520c46aaa711a
VladimirBelyaev <vogisa@mail.ru>
2024-12-25 01:11:36 +0300

    init: init styles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import '@/assets/style/index.scss';
+
 import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+
+import App from '@/App.vue';
 
 const app = createApp(App);
 
