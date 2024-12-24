e75a9340c5317149c9183fdf4497d430732b1f5e
VladimirBelyaev <vogisa@mail.ru>
2024-12-24 19:52:59 +0300

    ref: some initial refactor

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,5 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ props.msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -33,6 +25,14 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
 
 <style scoped>
 .read-the-docs {
