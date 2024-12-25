a132b7a62950eae806db099ac7b3b281d28abf3c
VladimirBelyaev <vogisa@mail.ru>
2024-12-25 17:09:38 +0300

    refactor(X3UI preview): changed order of blocks and refactor buttons preview

--- src/components/X3UIPreview.vue
+++ src/components/X3UIPreview.vue
@@ -1,26 +1,6 @@
 <template>
 	<div class="mt-4">
 		<div class="flex flex-col gap-4">
-			<span class="h1">Button</span>
-			<div v-for="(buttonType, index) in ButtonTypes" :key="index" class="flex flex-col gap-2">
-				<span class="h4">{{ startCase(buttonType) }}</span>
-				<div class="flex gap-2 items-start">
-					<X3Button v-for="(size, index) in sizes" :key="index" :type="buttonType" :size="size">Button</X3Button>
-				</div>
-			</div>
-			<span class="h1">Input</span>
-			<div v-for="(inputStatus, index) in InputStatuses" :key="index" class="flex flex-col gap-2">
-				<span class="h4">{{ startCase(inputStatus) }}</span>
-				<div class="flex gap-2 items-start">
-					<X3Input
-						v-for="(size, index) in sizes"
-						:key="index"
-						v-model:value="inputRef"
-						:size="size"
-						:status="inputStatus !== 'default' ? inputStatus : undefined"
-					/>
-				</div>
-			</div>
 			<span class="h1">Card</span>
 			<div class="flex gap-2 items-start">
 				<X3Card v-for="(size, index) in sizes" :key="index" :size="size">
@@ -39,6 +19,50 @@
 				<X3Button type="primary" @click="showModals.large = true">Show large modal</X3Button>
 			</div>
 			<X3Modal v-for="(size, index) in sizes" :key="index" v-model:show="showModals[size]" :size="size" title="Modal" />
+			<span class="h1">Button</span>
+			<div class="grid grid-cols-2 gap-2">
+				<X3Card>
+					<div class="flex flex-col gap-2">
+						<span class="h4">Basic</span>
+						<div class="flex gap-2 flex-wrap">
+							<X3Button v-for="(buttonType, index) in ButtonTypes" :key="index" :type="buttonType">Button</X3Button>
+						</div>
+					</div>
+				</X3Card>
+				<X3Card>
+					<div class="flex flex-col gap-2">
+						<span class="h4">Secondary</span>
+						<div class="flex gap-2 flex-wrap">
+							<X3Button v-for="(buttonType, index) in ButtonTypes" :key="index" :type="buttonType" secondary>
+								Button
+							</X3Button>
+						</div>
+					</div>
+				</X3Card>
+				<X3Card>
+					<div class="flex flex-col gap-2">
+						<span class="h4">Sized</span>
+						<div class="flex gap-2 flex-wrap">
+							<X3Button v-for="(size, index) in sizes" :key="index" :size="size">
+								Button
+							</X3Button>
+						</div>
+					</div>
+				</X3Card>
+			</div>
+			<span class="h1">Input</span>
+			<div v-for="(inputStatus, index) in InputStatuses" :key="index" class="flex flex-col gap-2">
+				<span class="h4">{{ startCase(inputStatus) }}</span>
+				<div class="flex gap-2 items-start">
+					<X3Input
+						v-for="(size, index) in sizes"
+						:key="index"
+						v-model:value="inputRef"
+						:size="size"
+						:status="inputStatus !== 'default' ? inputStatus : undefined"
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
