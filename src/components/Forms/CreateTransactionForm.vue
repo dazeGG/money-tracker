42eaede8c9ad207698f08efe30e69a158c32cde0
TimurFedorov <fkgohy@gmail.com>
2024-12-25 18:43:07 +0300

    feat(create transaction): created add transaction form

--- src/components/Forms/CreateTransactionForm.vue
+++ src/components/Forms/CreateTransactionForm.vue
@@ -1,8 +1,14 @@
 <template>
 	<div class="flex flex-col gap-2">
-		Form
+		<span>Title</span>
+		<X3Input v-model:value="model.title" placeholder="Transaction title" />
+		<span>Sum</span>
+		<X3Input v-model:value="model.sum" placeholder="Transaction sum" />
 	</div>
 </template>
 
 <script setup lang="ts">
+import X3Input from '@/X3UI/Input/X3Input.vue';
+
+const model = defineModel<{title: string; sum: string}>('model', { required: true });
 </script>
--- src/components/Modals/CreateTransactionModal.vue
+++ src/components/Modals/CreateTransactionModal.vue
@@ -1,12 +1,23 @@
 <template>
 	<X3Modal v-model:show="show" title="Add a transaction">
-		<CreateTransactionForm />
+		<template #default>
+			<CreateTransactionForm v-model:model="model" />
+		</template>
+		<template #footer>
+			<div class="flex justify-end">
+				<X3Button type="primary">Add</X3Button>
+			</div>
+		</template>
 	</X3Modal>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import X3Modal from '@/X3UI/Modal/X3Modal.vue';
+import X3Button from '@/X3UI/Button/X3Button.vue';
 import CreateTransactionForm from '@/components/Forms/CreateTransactionForm.vue';
 
 const show = defineModel<boolean>('show', { default: false });
+const model = ref<{title: string; sum: string}>({ title: '', sum: '' });
 </script>
