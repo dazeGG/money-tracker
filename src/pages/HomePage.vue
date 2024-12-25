ffe2e106150dd08e82625654ba7906cc2b7c1202
VladimirBelyaev <vogisa@mail.ru>
2024-12-25 19:11:03 +0300

    feat(home page): added create transaction modal

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,16 @@
 <template>
-	<h1 class="h1">Home Page</h1>
+	<div class="flex justify-between">
+		<CreateTransactionModal v-model:show="showCreateTransactionModal" />
+		<h1 class="h1">Home Page</h1>
+		<X3Button @click="() => showCreateTransactionModal = true">Add transaction</X3Button>
+	</div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import X3Button from '@/X3UI/Button/X3Button.vue';
+import CreateTransactionModal from '@/components/Modals/CreateTransactionModal.vue';
+
+const showCreateTransactionModal = ref<boolean>(false);
 </script>
